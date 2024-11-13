04d2e6b60c0d15432b75a3d2ed54f5a3416730ba
Gywem <igcogi@gmail.com>
2024-11-13 17:03:39 +0100

    ensure url package compatibility after latest upgrades of core-js

--- packages/url/legacy.js
+++ packages/url/legacy.js
@@ -1,6 +1,13 @@
 try {
+  require("url-search-params-polyfill");
+  URLSearchParams = window.URLSearchParams;
+  exports.URLSearchParams = URLSearchParams;
+
   require("core-js/proposals/url");
+  URL = window.URLSearchParams;
+  exports.URL = URL;
 } catch (e) {
+  console.log(' error', e);
   throw new Error([
     "The core-js npm package could not be found in your node_modules ",
     "directory. Please run the following command to install it:",
